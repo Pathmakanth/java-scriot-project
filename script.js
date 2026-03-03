c51c40163acdf9e2bc50778002fbbf941d9aa308
Pathmakanth <pathmakanthwings@gmail.com>
2026-03-03 21:18:21 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = "Naveen";
+let age = 22;
+let city = "Bangalore";
+
+console.log("My name is " + name + ".");
+console.log("I am " + age + " years old.");
+console.log("I am from " + city + ".");
